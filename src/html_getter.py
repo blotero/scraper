6b87a3b40b5ac89bc8f1e3f994468b27dc7ced57
Brandon Lotero <blotero@gmail.com>
2023-03-13 17:15:59 -0500

    Succesfully implemented recursive translator

--- src/html_getter.py
+++ src/html_getter.py
@@ -1,36 +1,22 @@
-import re
 from bs4 import BeautifulSoup
-import json
 from libretranslatepy import LibreTranslateAPI
 
-translator_api = "http://44.201.42.242:5000/"
-translator = LibreTranslateAPI(translator_api)
+def translateHtmlFile(path):
+    path = path
+    print(f"Getting strings for {path}")
+    with (open(path)) as f:
+        html_text = f.read()
+        soup = BeautifulSoup(html_text, 'html.parser')
+        elements = {}
+        interest_items = ['br', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'i', 'a']
+        for key in interest_items:
+            tags = soup.findAll(key)
+            for tag in tags:
+                # print(f"Original: {tags[i].string}")
+                translated = translator.translate(tag.string, 'en', 'hi') if tag.string else None
+                if translated:
+                    print(f"Translated: {translated}")
+                    html_text = html_text.replace(tag.string,translated)
 
-class HtmlTextGetter:
-    def __init__(self, path):
-        self.path = path
-    def translate(self):
-        print(f"Getting strings for {path}")
-        with (open(path)) as f:
-            html_text = f.read()
-            soup = BeautifulSoup(html_text, 'html.parser')
-            elements = {}
-            interest_items = ['br', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'i', 'a']
-            for key in interest_items:
-                tags = soup.findAll(key)
-                for tag in tags:
-                    # print(f"Original: {tags[i].string}")
-                    translated = translator.translate(tag.string, 'en', 'hi') if tag.string else None
-                    if translated:
-                        print(f"Translated: {translated}")
-                        html_text = html_text.replace(tag.string,translated)
-
-            with(open("output.html", "w")) as fout:
-                fout.write(html_text)
-                fout.close()
-
-
-if __name__ == '__main__':
-    path = "/home/brandon/My Web Sites/classcentral/index.html"
-    htg = HtmlTextGetter(path)
-    htg.translate()
+        f.close()
+        return html_text
--- src/page_translator.py
+++ src/page_translator.py
@@ -1,15 +1,15 @@
 from libretranslatepy import LibreTranslateAPI
 from path_utils import list_html_files,list_non_html_files
 from googletrans import Translator
+from bs4 import BeautifulSoup
 import shutil
 import os
 
 
 class RecursiveHtmlTranslator:
-    def __init__(self, root_path, translator_api, type):
+    def __init__(self, root_path, translator_api ):
         self.root_path = root_path
-        self.translator = LibreTranslateAPI(translator_api) if type == 'libre' else Translator()
-        self.type = type
+        self.translator = LibreTranslateAPI(translator_api)
 
     def copy_meta_files(self,output_dir):
         non_html_files = list_non_html_files(self.root_path)
@@ -18,48 +18,39 @@ class RecursiveHtmlTranslator:
             os.makedirs(os.path.dirname(dst), exist_ok=True)
             shutil.copyfile(file, dst)
 
-
+    def translate_from_root_dir(self, output_dir):
+        html_files = list_html_files(self.root_path)
+        for i,file in enumerate(html_files):
+            end_name = file.replace(self.root_path, output_dir)
+            print(f"Writing: {end_name}... ({i}/{len(html_files)})")
+            translated_content = self.translateHtmlFile(file)
+            os.makedirs(os.path.dirname(file), exist_ok=True)
+            f_out = open(end_name, "w")
+            f_out.write(translated_content)
+            f_out.close()
 
     def start(self, output_dir):
         print("Copying meta files...")
         self.copy_meta_files(output_dir)
-        print("Copying html files...")
-        html_files = list_html_files(self.root_path)
-        for file in html_files:
-            with open(file) as f:
-                print(f"Translating: {file} with {self.type}...")
-                contentlines= f.readlines()
-
-                for i,content in enumerate(contentlines):
-                    if self.type == 'libre':
-                        translated_content = self.translator.translate(content, 'en', 'hi')
-                    
-                    if self.type == 'google':
-                        translated_content = self.translator.translate(content, src='en', dest='hi')
-
-                    end_name = file.replace(self.root_path, output_dir)
-                    print(f"Writing: {end_name} (line {i} of {len(content)})...")
-
-                    f_out = open(end_name, "a")
-                    f_out.write(translated_content)
-                    f_out.close()
+        print("Translating html files...")
+        self.translate_from_root_dir(output_dir)
 
     
-    def get_strings(self,html_text ):
-        print(f"Getting strings for {path}")
-        soup = BeautifulSoup(html_text, 'html.parser')
-        elements = {}
-        interest_items = ['br', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'i', 'a']
-        for key in interest_items:
-            elements[key] = soup.find_all(key)
-        self.elements = elements
+    def translateHtmlFile(self,path):
+        with (open(path)) as f:
+            html_text = f.read()
+            soup = BeautifulSoup(html_text, 'html.parser')
+            elements = {}
+            interest_items = ['br', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'i', 'a',
+                              'label','span']
+            for key in interest_items:
+                tags = soup.findAll(key)
+                for tag in tags:
+                    translated = self.translator.translate(tag.string, 'en', 'hi') if tag.string else None
+                    if translated:
+                        # print(f"Original: {tag.string}")
+                        # print(f"Translated: {translated}")
+                        html_text = html_text.replace(tag.string,translated)
 
-    def translate(self):
-        for key,value_list in self.elements.items():
-            print(f"Translating items from {key}...")
-            for value in value_list:
-                print(f"Original: {value}")
-                translated = translator.translate(value, 'en', 'hi')
-                print(f"Translated: {translated}")
-
-                    
+            f.close()
+            return html_text
